babf3c19de31fb796d220af43e318ad5566bd7fa
Carl Swanson <cetswanson@gmail.com>
2015-06-02 19:25:54 -0400

    Add scroll position function

--- WiffleChat.js
+++ WiffleChat.js
@@ -10,6 +10,10 @@ if (Meteor.isClient) {
       }
   });
 
+  var scrollPosition = function() {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  };
+
   Template.input.events = {
     'keydown input#message' : function (event) {
       if (event.which == 13) { // 13 is the enter key event
