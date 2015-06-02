06b311b4198fafcc690f9c03293d9399b0f7bec9
Carl Swanson <cetswanson@gmail.com>
2015-06-02 14:44:08 -0400

    Add message template helper

--- WiffleChat.js
+++ WiffleChat.js
@@ -6,6 +6,12 @@ if (Meteor.isClient) {
      passwordSignupFields: 'USERNAME_ONLY'
   });
 
+  Template.messages.helpers({
+      messages: function() {
+          return Messages.find({}, { sort: { time: -1}});
+      }
+  });
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
