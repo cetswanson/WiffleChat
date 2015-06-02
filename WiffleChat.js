2899b3ea21e8bc711bae817c5c8de0c5e099755d
Carl Swanson <cetswanson@gmail.com>
2015-06-02 14:44:24 -0400

    Add message input js

--- WiffleChat.js
+++ WiffleChat.js
@@ -12,6 +12,29 @@ if (Meteor.isClient) {
       }
   });
 
+  Template.input.events = {
+    'keydown input#message' : function (event) {
+      if (event.which == 13) { // 13 is the enter key event
+        if (Meteor.user())
+          var name = Meteor.user().username;
+        else
+          var name = 'Anonymous';
+        var message = document.getElementById('message');
+
+        if (message.value != '') {
+          Messages.insert({
+            name: name,
+            message: message.value,
+            time: Date.now(),
+          });
+
+          document.getElementById('message').value = '';
+          message.value = '';
+        }
+      }
+    }
+  };
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
