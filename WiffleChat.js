519842dfb4c6a67dee6c6be6157359abca97d9cd
Carl Swanson <cetswanson@gmail.com>
2015-06-03 17:27:20 -0400

    Refactor sendMessage function

--- WiffleChat.js
+++ WiffleChat.js
@@ -18,28 +18,39 @@ if (Meteor.isClient) {
     $(".messageBox").prop({ scrollTop: $(".messageBox").prop("scrollHeight") });
   };
 
+  $(document).ready(function(){
+    scrollPosition();
+  });
+
+  var sendMessage = function() {
+    if (Meteor.user())
+      var name = Meteor.user().username;
+    else
+      var name = 'Anonymous';
+    var message = document.getElementById('message');
+
+    if (message.value != '') {
+      Messages.insert({
+        name: name,
+        message: message.value,
+        time: Date.now(),
+      });
+
+      document.getElementById('message').value = '';
+      message.value = '';
+      scrollPosition();
+    }
+  };
+
   Template.input.events = {
     'keydown input#message' : function (event) {
       if (event.which == 13) {
-        if (Meteor.user())
-          var name = Meteor.user().username;
-        else
-          var name = 'Anonymous';
-        var message = document.getElementById('message');
-
-        if (message.value != '') {
-          Messages.insert({
-            name: name,
-            message: message.value,
-            time: Date.now(),
-          });
-
-          document.getElementById('message').value = '';
-          message.value = '';
-          scrollPosition();
-        }
+        sendMessage();
       }
-    }
+     },
+      'click #sendMessage': function(event) {
+        sendMessage();
+     }
   };
 
 if (Meteor.isServer) {
